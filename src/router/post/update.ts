2f8e0aa7d2d24c33872b3757b0475473d076d76d
feranmi <feranmiolasunkanmi91@gmail.com>
2023-11-06 17:41:56 +0100

    working on post update

---
+++ src/router/post/update.ts
@@ -0,0 +1,28 @@
+import { Request, Response, Router, NextFunction } from "express";
+import Post from "src/models/post";
+
+const router = Router();
+
+router.post('/api/post/update/:id', async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params
+    const { title, content } = req.body
+    if (!id) {
+        const error = new Error('Post id is required') as CustomError
+        error.status = 400;
+        next(error)
+    } 
+    let updatedPost;
+    try {
+        updatedPost = await Post.findOneAndUpdate(
+            { _id: id },
+            { $set: {content, title} },
+            { new: true }
+        )
+    } catch (err) {
+        const error = new Error('Post Cannot be Updated') as CustomError
+        error.status = 400
+        next(error)
+    }
+    res.status(200).send(updatedPost)
+})
+export { Router as updatePostRouter }
